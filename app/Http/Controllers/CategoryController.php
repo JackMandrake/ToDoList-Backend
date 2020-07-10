7961c840308b68cae4d708ca2299ad388b06334f
JackMandrake <coignard.cyrille@hotmail.fr>
2020-07-10 14:28:24 +0200

    première route avec Eloquent

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,39 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Log;
+use App\Models\Category;
 
-class CategoryController  extends Controller {
+class CategoryController extends Controller {
 
     /**
      * HTTP Method : GET
      * URL : /categories
      */
     public function list() {
-        $categoriesList = [
-            1 => [
-              'id' => 1,
-              'name' => 'Chemin vers O\'clock',
-              'status' => 1
-            ],
-            2 => [
-              'id' => 2,
-              'name' => 'Courses',
-              'status' => 1
-            ],
-            3 => [
-              'id' => 3,
-              'name' => 'O\'clock',
-              'status' => 1
-            ],
-            4 => [
-              'id' => 4,
-              'name' => 'Titre Professionnel',
-              'status' => 1
-            ]
-        ];
+
+        $categoriesList = Category::all();
+        // dump($categoriesList);
 
         // On renvoie le tableau des catégories en JSON
-        return response()->json($categoriesList);
+        // avec un code de retour HTTP à 200
+        return $this->sendJsonResponse($categoriesList, 200);
     }
 
     /**
